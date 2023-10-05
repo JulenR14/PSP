2c6a253209f84d157051574f691459a928cfaa99
JulenR14 <julen20191414@gmail.com>
2023-10-05 17:06:05 +0200

    Programa procesos

---
+++ Programas/LanzadorProcesos/src/main/java/org/example/LanzadorProcesos.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class LanzadorProcesos {
+    public void ejecutar(String ruta){
+
+        ProcessBuilder pb;
+        try{
+            pb = new ProcessBuilder(ruta);
+            pb.start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
