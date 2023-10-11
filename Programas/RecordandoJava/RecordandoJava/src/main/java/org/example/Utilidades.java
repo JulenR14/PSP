47af80f9b5aff670849f20f965788be90f3d799e
JulenR14 <julen20191414@gmail.com>
2023-10-11 18:40:56 +0200

    Comentarios añaidos

--- Programas/RecordandoJava/RecordandoJava/src/main/java/org/example/Utilidades.java
+++ Programas/RecordandoJava/RecordandoJava/src/main/java/org/example/Utilidades.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Utilidades {
 
+    //metodo que recibe un array de integers y devuelve el array completo de numeros(10% mas elevado que permite java)
     public static int[] rellenarArrayNumerosAleatorios(int[] numeros){
+        //recorremos un for para completarlo
         for (int contador = 0; contador < numeros.length; contador++){
             numeros[contador] = (int)(Math.random() * ((Integer.MAX_VALUE - (Integer.MAX_VALUE*0.1))+1));
         }
