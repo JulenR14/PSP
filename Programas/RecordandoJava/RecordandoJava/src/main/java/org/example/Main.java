2c84f7fd875f154f3302f43af832e2eda5246f8a
JulenR14 <julen20191414@gmail.com>
2023-10-11 20:03:40 +0200

    Programa terminado

--- Programas/RecordandoJava/RecordandoJava/src/main/java/org/example/Main.java
+++ Programas/RecordandoJava/RecordandoJava/src/main/java/org/example/Main.java
@@ -1,16 +1,34 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
 
         //isntanciacion de los dos arrays de 10 posiciones
-        int[] diezNumeros = new int[10];
-        Boolean[] diezBooleanos = new Boolean[10];
+        int[] diezNumeros = new int[100];
+
+        long start = System.nanoTime();
+
+        Utilidades.rellenarArrayNumerosAleatorios(diezNumeros);
 
         //llamamos al metodo que devuelve el array rellenado y lo recorremos
-        Arrays.stream(Utilidades.rellenarArrayNumerosAleatorios(diezNumeros))
-                .forEach(System.out::println);
+        Arrays.stream(diezNumeros).forEach(numero -> {
+            if(Utilidades.naive(numero)){
+                System.out.println("Numero - " + numero + " - Es primo.");
+            }else{
+                System.out.println("Numero - " + numero + " - No es primo.");
+            }
+        });
+
+        long end = System.nanoTime();
+
+
+        double diff = end-start;
+
+        System.out.println("Segundos : " + String.format("%.3f", (diff/1000000000))
+                + ", Milisegundos : " + (diff/1000000)
+                + ", Nanosegundos : " + diff);
     }
 }
--- Programas/RecordandoJava/RecordandoJava/src/main/java/org/example/Utilidades.java
+++ Programas/RecordandoJava/RecordandoJava/src/main/java/org/example/Utilidades.java
@@ -6,10 +6,23 @@ public class Utilidades {
     public static int[] rellenarArrayNumerosAleatorios(int[] numeros){
         //recorremos un for para completarlo
         for (int contador = 0; contador < numeros.length; contador++){
-            numeros[contador] = (int)(Math.random() * ((Integer.MAX_VALUE - (Integer.MAX_VALUE*0.1))+1));
+            numeros[contador] = (int)(Math.random() * ((Integer.MAX_VALUE - (Integer.MAX_VALUE*0.9))+1));
         }
 
         return numeros;
+    }
+
+    //metodo que devuelve si el numero es primo o no
+    public static boolean naive(int numero){
+        boolean primo = true;
+
+        for (int i = 2; i < numero; i++){
+            if(numero % i == 0){
+                primo = false;
+            }
+        }
+
+        return primo;
     }
 
 }
