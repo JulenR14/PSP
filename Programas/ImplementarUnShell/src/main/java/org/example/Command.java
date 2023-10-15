200a5e072f9b91fb96cc4f2d06210edbb2cf818d
JulenR14 <julen20191414@gmail.com>
2023-10-15 20:27:56 +0200

    cambios

--- Programas/ImplementarUnShell/src/main/java/org/example/Command.java
+++ Programas/ImplementarUnShell/src/main/java/org/example/Command.java
@@ -1,16 +1,13 @@
 package org.example;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.SimpleTimeZone;
 
 public class Command {
     //Atributos
     String[] comandos;
-    String ficheroSalida = "";
+    String ficheroSalida;
 
     //Constructores
     public Command(String[] comandos, String ficheroSalida){
