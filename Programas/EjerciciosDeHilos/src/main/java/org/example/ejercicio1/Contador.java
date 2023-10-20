8fad37a10f881247d69cff85ea51ef57054b7416
JulenR14 <julen20191414@gmail.com>
2023-10-20 20:21:44 +0200

    Iniciando Ejercicio Hilos

---
+++ Programas/EjerciciosDeHilos/src/main/java/org/example/ejercicio1/Contador.java
@@ -0,0 +1,14 @@
+package org.example.ejercicio1;
+
+public class Contador {
+
+    private int contador;
+
+    public void incrementarContador(){
+        contador++;
+    }
+
+    public int getContador(){
+        return contador;
+    }
+}
---
+++ Programas/EjerciciosDeHilos/src/main/java/org/example/ejercicio2/Main.java
@@ -0,0 +1,13 @@
+package org.example.ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Thread liebre;
+        Thread tortuga;
+        Thread caballo;
+        Thread perro;
+
+
+    }
+}
