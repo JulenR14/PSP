ba1326c814b5ac4abf1c217a4d71fa3c288178cc
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2023-11-29 16:47:58 +0100

    Base del primer ejercicio de servidores

---
+++ Programas/Aplicacion1_time/src/UDPClient.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.*;
+
+public class UDPClient {
+    public static void main(String[] args) {
+
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            byte[] mensaje = "Hola".getBytes();
+            InetAddress serverHost = InetAddress.getByName("localhost");
+            int serverPort = 6789;
+
+            DatagramPacket sendDatagram = new DatagramPacket(mensaje, mensaje.length, serverHost, serverPort);
+
+            socket.send(sendDatagram);
+
+            byte[] respuesta = new byte[1000];
+            DatagramPacket respuestaDatagram = new DatagramPacket(respuesta, respuesta.length);
+            socket.receive(respuestaDatagram);
+
+            System.out.println("La respuesta del servidor es: " + new String(respuestaDatagram.getData()));
+
+            socket.close();
+
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
