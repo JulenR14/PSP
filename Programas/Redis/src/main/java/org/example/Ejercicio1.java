08e308584e6059e7eea326b8b3608bc56f7b3e42
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2023-12-13 22:23:54 +0100

    Falta pulir bucle

--- Programas/Redis/src/main/java/org/example/Ejercicio1.java
+++ Programas/Redis/src/main/java/org/example/Ejercicio1.java
@@ -12,11 +12,14 @@ import java.util.List;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Ejercicio1 {
     public static void main(String[] args) throws IOException {
-        Jedis jedis = new Jedis("34.228.162.124", 6379);
 
+        //claves para almacenar la listas de urls
         String clave_URLTOSHORT = "JULEN:URLS_TO_SHORT";
         String clave_SHORTEDURL = "JULEN:SHORTED_URLS";
 
+
+
+        //pedimos el comando al usuario
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Escribe el comando que quieras :\n" +
                 "- shorten URL : URL es la url a acortar\n" +
@@ -25,33 +28,53 @@ public class Ejercicio1 {
         System.out.print("Escribe el comando que quieras :");
         String comando = bf.readLine();
 
+        Thread acortador = new Thread(new Ejercicio2());
 
-        jedis.del(clave_URLTOSHORT);
-        while (!comando.equals("exit")) {
-            String[] comandoSplit = comando.split(" ");
-            if (comandoSplit[0].equals("shorten")) {
-                String url = comandoSplit[1];
-                jedis.lpush(clave_URLTOSHORT, url);
-                String[] separados = url.split("/");
+        //conectamos con el servidor de redis
+        try(Jedis jedis = new Jedis("34.228.162.124", 6379)) {
+            //iniciamos el hilo
+            acortador.start();
+            //borramos las claves para que no haya problemas
+            jedis.del(clave_URLTOSHORT);
+            while (!comando.equals("exit")) {
+                //separamos el comando en palabras
+                String[] comandoSplit = comando.split(" ");
+                //comprobamos cual es el inicio del comando
+                if (comandoSplit[0].equals("shorten")) {
+                    String url = comandoSplit[1];
+                    //añadimos la url a la lista
+                    jedis.lpush(clave_URLTOSHORT, url);
+                    System.out.println("En proceso de acortar la url");
+                    //acortamos la url
+                /*String[] separados = url.split("/");
                 String shortedUrl = "";
                 for(int i = 0; i < (separados.length-1); i++){
                     shortedUrl += separados[i] + "/";
                 }
+                //añadimos la url acortada al hash, la clave es la url acortada y el valor es la url
+                //de esta forma cuando quiera buscar la url sera mas facil
                 jedis.hset(clave_SHORTEDURL, shortedUrl, url);
-                System.out.println("La url acortada es : " + shortedUrl);
-            } else if (comandoSplit[0].equals("url")) {
-                String shortedUrl = comandoSplit[1];
-                String url = jedis.hget(clave_SHORTEDURL, shortedUrl);
-                if (url == null) {
-                    System.out.println("La url no existe");
+                System.out.println("La url acortada es : " + shortedUrl);*/
+                } else if (comandoSplit[0].equals("url")) {
+                    String shortedUrl = comandoSplit[1];
+                    //buscamos la url acortada en el hash
+                    String url = jedis.hget(clave_SHORTEDURL, shortedUrl);
+                    //comprobamos que la url existe
+                    if (url == null) {
+                        System.out.println("La url no existe");
+                    } else {
+                        //si existe la mostramos
+                        System.out.println("La url es : " + url);
+                    }
                 } else {
-                    System.out.println("La url es : " + url);
+                    System.out.println("Comando no valido");
                 }
-            } else {
-                System.out.println("Comando no valido");
+                System.out.print("Escribe el comando que quieras :");
+                comando = bf.readLine();
             }
-            System.out.print("Escribe el comando que quieras :");
-            comando = bf.readLine();
+            acortador.join();
+        }catch (Exception e){
+            System.out.println("Error al conectar con la base de datos");
         }
 
     }
---
+++ Programas/Redis/src/main/java/org/example/Ejercicio2.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import redis.clients.jedis.Jedis;
+
+public class Ejercicio2 implements Runnable {
+    @Override
+    public void run() {
+        String clave_URLTOSHORT = "JULEN:URLS_TO_SHORT";
+        String clave_SHORTEDURL = "JULEN:SHORTED_URLS";
+
+        try(Jedis jedis = new Jedis("34.228.162.124", 6379);){
+            while(true){
+                //eliminamos la ultima url de la lista y la acortamos metiendola en el hash
+                String url = jedis.rpop(clave_URLTOSHORT);
+                if(url != null){
+                    String[] separados = url.split("/");
+                    String shortedUrl = "";
+                    for(int i = 0; i < (separados.length-1); i++){
+                        shortedUrl += separados[i] + "/";
+                    }
+                    jedis.hset(clave_SHORTEDURL, shortedUrl, url);
+                    System.out.println("La url acortada es : " + shortedUrl);
+                }
+
+            }
+        }
+    }
+}
